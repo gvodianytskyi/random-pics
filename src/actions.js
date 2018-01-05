f590d809fcf4a3a99cf6e8bc368a60520d6eb40a
Gennadii Vodianytskyi <gennadii_vodianytskyi@epam.com>
2018-01-05 21:27:47 +0200

    fixed bugs

--- src/actions.js
+++ src/actions.js
@@ -18,6 +18,13 @@ export const clearPics = () => ({
 });
 
 export const getPics = () => (dispatch) => {
+    // axios.post(url, {
+    //     headers: {'Access-Control-Allow-Origin': '*',
+    //         'Content-Type': 'application/json;charset=UTF-8',
+    //         'Access-Control-Allow-Methods': 'DELETE, HEAD, GET, OPTIONS, POST, PUT',
+    //         'Access-Control-Allow-Headers': 'Content-Type, Content-Range, Content-Disposition, Content-Description'
+    //     }
+    // })
     axios(url)
         .then((responseBody) => {
             dispatch(loadPics(responseBody.data.data.children.slice(1)));
@@ -31,10 +38,9 @@ export const getPics = () => (dispatch) => {
 
 export const loadMorePics = () => (dispatch) => {
     const newUrl = `${url}&after=${after}`;
-    console.log('newUrl: ', newUrl);
     axios(newUrl)
         .then((responseBody) => {
-            dispatch(appendPics(responseBody.data.data.children.slice(1)));
+            dispatch(appendPics(responseBody.data.data.children));
             after = responseBody.data.data.after;
         })
         .catch(error => {
